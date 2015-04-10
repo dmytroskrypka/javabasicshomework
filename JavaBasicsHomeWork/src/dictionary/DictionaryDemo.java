db528f34a23061236774178af681f25e1eb74d01
dmytroskrypka <dmytroskrypka@users.noreply.github.com>
2015-04-11 00:44:41 +0300

    Solved dictionary task (HashMap lesson homework)

---
+++ JavaBasicsHomeWork/src/dictionary/DictionaryDemo.java
@@ -0,0 +1,9 @@
+package dictionary;
+
+public class DictionaryDemo {
+
+	public static void main(String[] args) {
+		System.out.println((new Dictionary()).translate("fish cat human test dog bird"));
+	}
+
+}
