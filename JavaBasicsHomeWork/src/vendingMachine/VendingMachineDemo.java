4bbea58eb3bead78cc0f802166d1d1c75689e647
dmytroskrypka <dmytroskrypka@users.noreply.github.com>
2015-04-12 01:04:17 +0300

    Minor refactoring in VendingMachineDemo class

--- JavaBasicsHomeWork/src/vendingMachine/VendingMachineDemo.java
+++ JavaBasicsHomeWork/src/vendingMachine/VendingMachineDemo.java
@@ -5,34 +5,34 @@ public class VendingMachineDemo {
 	
 
 	public static void main(String[] args) {
-		//VendingMachine wm = new VendingMachine(); //create new machine
-		//wm.saveToFile(VMDATAFILE);
-		
-		VendingMachine wm = new VendingMachine(VMDATAFILE); //read existing machine data from file
-		
+		VendingMachine wm = new VendingMachine(VMDATAFILE);
+		VendingMachineDemo.operate(wm);
+	}
+	
+	public static void operate(VendingMachine wm){
 		while (true){
-	        	System.out.println("1 - see menu");
-	        	System.out.println("2 - make order");
-	        	System.out.println("3 - supply drinks");
-	        	System.out.println("0 - exit");
-	        
-	        	switch (wm.getScanner().nextInt()){
-		     	   	case 1:
-		    			System.out.println(wm.toString());
-		    			break;
-		    	    	case 2:
-		     	   		wm.payForOrder(wm.selectOrder());//try with lastorder
-			        	break;
-			        case 3:
-			        	wm.addUserProductQuantity();
-			        	break;
-			        case 0:
-			    		System.out.println("Have a nice day!");
-			    		wm.saveToFile(VMDATAFILE);
-			        	return;
-			        default:
-			        	System.out.println("Input mismatch, please try again");
-	        	}
+        	System.out.println("1 - see menu");
+        	System.out.println("2 - make order");
+        	System.out.println("3 - supply drinks");
+        	System.out.println("0 - exit");
+        
+        	switch (wm.getScanner().nextInt()){
+	     	   	case 1:
+	    			System.out.println(wm.toString());
+	    			break;
+	    	    	case 2:
+	     	   		wm.payForOrder(wm.selectOrder());
+		        	break;
+		        case 3:
+		        	wm.addUserProductQuantity();
+		        	break;
+		        case 0:
+		    		System.out.println("Have a nice day!");
+		    		wm.saveToFile(VMDATAFILE);
+		        	return;
+		        default:
+		        	System.out.println("Input mismatch, please try again");
+        	}
 		}
 	}
 
