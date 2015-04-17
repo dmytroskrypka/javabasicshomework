0bbc6b6f525558c5eb8c56ebca1ada475dec4018
edmiskr <dmytro.skrypka@github.com>
2015-04-17 16:31:58 +0300

    Added task 4 synchronization with AtomicInteger

---
+++ JavaBasicsHomeWork/synchronization/_threads_synchronization_4/Account.java
@@ -0,0 +1,21 @@
+package _threads_synchronization_4;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Account {
+	AtomicInteger money;
+	final int MIN_ALLOWED_BALANCE;
+	
+	public Account(AtomicInteger money, int min_allowed_balance) {
+		this.money = money;
+		this.MIN_ALLOWED_BALANCE = min_allowed_balance;
+	}
+	
+	public AtomicInteger get() {
+		return money;
+	}
+	
+	public void set(AtomicInteger x) {
+		money = x;
+	}
+}
