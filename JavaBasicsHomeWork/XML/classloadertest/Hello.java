09e2df9f3e4b151ce2d75f821bb7879cf87b9338
dmytroskrypka <dmytroskrypka@users.noreply.github.com>
2015-04-20 22:04:49 +0300

    added classloader test

--- JavaBasicsHomeWork/XML/classloadertest/Hello.java
+++ JavaBasicsHomeWork/XML/classloadertest/Hello.java
@@ -6,8 +6,8 @@ import java.io.*;
 public class Hello {
 
 	public static void main(String[] args) {
-		InputStream in = Hello.class.getClass().getClassLoader().getResourceAsStream("json.jsn");
-		System.out.println(in!=null); //not working!!!
+		InputStream in = Hello.class.getResourceAsStream("json.jsn");
+		System.out.println(in!=null); 
 		//BufferedReader r = new BufferedReader(new InputStreamReader(in));
 		
 	  }
