2b1ed57d2a0537a372e49b664fc7c85879da9bb3
edmiskr <dmytro.skrypka@github.com>
2015-04-21 16:29:10 +0300

    reverting previous gitignore

--- JavaBasicsHomeWork/XML/classloadertest/Hello.java
+++ JavaBasicsHomeWork/XML/classloadertest/Hello.java
@@ -6,7 +6,7 @@ import java.io.*;
 public class Hello {
 
 	public static void main(String[] args) {
-		InputStream in = Hello.class.getResourceAsStream("json.jsn");
+		InputStream in = Hello.class.getResourceAsStream("classloader.txt");
 		System.out.println(in!=null); 
 		//BufferedReader r = new BufferedReader(new InputStreamReader(in));
 		
